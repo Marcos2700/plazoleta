62e7340e971e9d1ffc03d31ddbc9c3a6b0ed3982
Marcos Escobar <marcos.escobar@pragma.com.co>
2023-08-10 12:27:46 -0500

    sonar

--- build/generated/sources/annotationProcessor/java/main/com/pragma/powerup/application/mapper/IRestaurantResponseMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/com/pragma/powerup/application/mapper/IRestaurantResponseMapperImpl.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-08-10T08:49:30-0500",
-    comments = "version: 1.5.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.5.jar, environment: Java 20.0.1 (Oracle Corporation)"
+    date = "2023-08-10T12:24:11-0500",
+    comments = "version: 1.5.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.5.jar, environment: Java 18.0.2 (Amazon.com Inc.)"
 )
 @Component
 public class IRestaurantResponseMapperImpl implements IRestaurantResponseMapper {
